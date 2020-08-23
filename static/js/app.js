0571a16f6db72dbe726fd8f1eb7a8d1c91b7c46d
Bincy Narath <bincybaburajnarath@gmail.com>
2020-08-23 13:34:40 -0400

    UFO-level1 table is updated

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
-
-// YOUR CODE HERE!
+//Select the table body element from html page
+tbody=d3.select('#ufo-table>tbody')
+//To make sure that data is not appended over and over
+tbody.html("")
+//Take out each row and display
+tableData.forEach(row => {
+    //Append the dable
+    //Append data in the order of City, State, Country, Shape, Duration and comments
+    //as td in the table body
+    tr=tbody.append('tr')
+    tr.append('td').html(row.datetime)
+    tr.append('td').html(row.city)
+    tr.append('td').html(row.state)
+    tr.append('td').html(row.country)
+    tr.append('td').html(row.shape)
+    tr.append('td').html(row.durationMinutes)
+    tr.append('td').html(row.comments)
+})
